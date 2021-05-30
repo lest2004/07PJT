ca190139558e240cba63c0ac7aa06abb3086f374
lest2004 <madlest2004@gmail.com>
2021-05-30 12:13:10 +0900

    New Git

---
+++ 07.Model2MVCShop(URI,pattern)/src/main/java/com/model2/mvc/web/product/ProductController.java
@@ -0,0 +1,119 @@
+package com.model2.mvc.web.product;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.model2.mvc.common.Page;
+import com.model2.mvc.common.Search;
+import com.model2.mvc.service.domain.Product;
+import com.model2.mvc.service.product.ProductService;
+
+
+
+//==> 회원관리 Controller
+@Controller
+@RequestMapping("/product/*")
+public class ProductController {
+	
+	///Field
+	@Autowired
+	@Qualifier("productServiceImpl")
+	private ProductService productService;
+	//setter Method 구현 않음
+		
+	public ProductController(){
+		System.out.println(this.getClass());
+	}
+	
+	@Value("#{commonProperties['pageUnit']}")
+	int pageUnit;
+	
+	@Value("#{commonProperties['pageSize']}")
+	int pageSize;
+	
+	
+	//@RequestMapping("/addUserView.do")
+	//public String addUserView() throws Exception {
+	@RequestMapping( value="addProduct", method=RequestMethod.GET )
+	public String addProduct() throws Exception{
+	
+		System.out.println("/product/addProduct : GET");
+		
+		return "redirect:/product/addProductView.jsp";
+	}
+	
+	//@RequestMapping("/addUser.do")
+	@RequestMapping( value="addProduct", method=RequestMethod.POST )
+	public String addProduct( @ModelAttribute("product") Product product ) throws Exception {
+
+		System.out.println("/product/addProduct : POST");
+		//Business Logic
+		productService.addProduct(product);
+		
+		return "redirect:/product/addProduct.jsp";
+	}
+	
+	//@RequestMapping("/getUser.do")
+	@RequestMapping( value="getProduct", method=RequestMethod.GET )
+	public String getProduct( @RequestParam("prodNo") int prodNo , Model model ) throws Exception {
+		
+		System.out.println("/user/getUser : GET");
+		//Business Logic
+		Product prod = productService.getProduct(prodNo);
+		// Model 과 View 연결
+		model.addAttribute("product", prod);
+		
+		return "forward:/product/getProduct.jsp";
+	}
+	
+	//@RequestMapping("/updateUserView.do")
+	//public String updateUserView( @RequestParam("userId") String userId , Model model ) throws Exception{
+	@RequestMapping( value="updateProduct", method=RequestMethod.GET )
+	public String updateProduct( @RequestParam("prodNo") int prodNo , Model model ) throws Exception{
+
+		System.out.println("/product/updateProduct : GET");
+		//Business Logic
+		Product prod = productService.getProduct(prodNo);
+		// Model 과 View 연결
+		model.addAttribute("product", prodNo);
+		
+		return "forward:/product/updateProduct.jsp";
+	}
+
+	//@RequestMapping("/listUser.do")
+	@RequestMapping( value="listProduct" )
+	public String listProduct( @ModelAttribute("search") Search search , Model model , HttpServletRequest request) throws Exception{
+		
+		System.out.println("/product/listProduct : GET / POST");
+		
+		if(search.getCurrentPage() ==0 ){
+			search.setCurrentPage(1);
+		}
+		search.setPageSize(pageSize);
+		
+		// Business logic 수행
+		Map<String , Object> map=productService.getProductList(search);
+		
+		Page resultPage = new Page( search.getCurrentPage(), ((Integer)map.get("totalCount")).intValue(), pageUnit, pageSize);
+		System.out.println(resultPage);
+		
+		// Model 과 View 연결
+		model.addAttribute("list", map.get("list"));
+		model.addAttribute("resultPage", resultPage);
+		model.addAttribute("search", search);
+		
+		return "forward:/product/listProduct.jsp";
+	}
+}
